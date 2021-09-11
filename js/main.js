7ce9a5362fe949babbae26b546e02cc210e2910f
Yeliazar <erik.ruzhakov@mail.ru>
2021-09-11 17:22:18 +0300

    Add Section Fairy tail

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(function(){
-
+    $('.fairy-tail-slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="./images/icons/back.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="./images/icons/next.svg" alt=""></button>',
+        autoplay: true,
+        fade: true
+    });
 });
