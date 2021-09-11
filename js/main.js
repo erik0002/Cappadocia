5222716892ae6af7e64a3482f210798c0808cd2c
Yeliazar <erik.ruzhakov@mail.ru>
2021-09-11 23:08:41 +0300

    Added section hotel and our trip

--- js/main.js
+++ js/main.js
@@ -5,4 +5,11 @@ $(function(){
         autoplay: true,
         fade: true
     });
+
+    $('.our-trip-slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="./images/icons/back.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="./images/icons/next.svg" alt=""></button>',
+        autoplay: true,
+        fade: true
+    });
 });
