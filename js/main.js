14d2705df41e2fb1e59ebbf29831d0dd26df7914
Yeliazar <erik.ruzhakov@mail.ru>
2021-09-12 23:53:04 +0300

    add adaptivity

--- js/main.js
+++ js/main.js
@@ -3,13 +3,29 @@ $(function(){
         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="./images/icons/back.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="./images/icons/next.svg" alt=""></button>',
         autoplay: true,
-        fade: true
+        fade: true,
+        responsive: [
+            {
+              breakpoint: 601,
+              settings: {
+                arrows: false
+              }
+            },
+          ]
     });
 
     $('.our-trip-slider').slick({
         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="./images/icons/back.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="./images/icons/next.svg" alt=""></button>',
         autoplay: true,
-        fade: true
+        fade: true,
+        responsive: [
+            {
+              breakpoint: 600,
+              settings: {
+                arrows: false
+              }
+            },
+          ]
     });
 });
